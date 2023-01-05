21041cf570c06684187c329c3c149778c1a19062
Brenno B. Araújo <bb_araujo@icloud.com>
2023-01-05 18:49:43 -0300

    SwiftUI aula-01

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -2,19 +2,72 @@
 //  ContentView.swift
 //  Calculator
 //
-//  Created by Murillo R. Araújo on 04/01/23.
+//  Created by Brenno B. Araújo on 04/01/23.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 0) {
+            HStack {
+                Button("7") {
+
+                }
+                .padding()
+                .frame(maxWidth:  .infinity)
+
+                Button("8") {
+
+                }
+                .padding()
+                .frame(maxWidth:  .infinity)
+
+                Button("9") {
+
+                }
+                .padding()
+                .frame(maxWidth:  .infinity)
+
+                Button("x") {
+
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background( Color.orange)
+            }
+            .foregroundColor(Color .white)
+
+            HStack {
+                Button("4") {
+
+                }
+                .padding()
+                .frame(maxWidth:  .infinity)
+
+                Button("5") {
+
+                }
+                .padding()
+                .frame(maxWidth:  .infinity)
+
+                Button("6") {
+
+                }
+                .padding()
+                .frame(maxWidth:  .infinity)
+
+                Button("-") {
+
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background( Color.orange)
+            }
+            .foregroundColor(Color .white)
         }
+        .frame(maxWidth: .infinity)
+        .background(Color.black)
         .padding()
     }
 }
